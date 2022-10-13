7d0601151ad76e2a9a65b40591d9ae0cadaa297d
Sarvinoz <fullstackdevelop2023@gmail.com>
2022-10-13 15:40:12 +0500

    Start of header design

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Router, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
 
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.scss";
+
+const Header = () => {
+    return <>
+
+    </>;
+};
+
+export default Header;
