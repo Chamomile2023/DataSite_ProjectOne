15baad8d0c677f1fcbfab986fd9ab28301e8db7d
Sarvinoz <fullstackdevelop2023@gmail.com>
2022-10-14 16:04:42 +0500

    Almost end of the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,35 +3,33 @@ import Button from "../Button/Button";
 import "./Header.scss";
 
 const Header = () => {
-  return (
-    <>
-      <header className="header">
-        <div className="container">
-          <div className="header__hero">
-            <nav className="header__nav">
-              <div className="header__toggle">
-                <div className="header__circle">
-                  <div className="header__circle--span"></div>
-                  <div className="header__circle--span1"></div>
-                  <div className="header__circle--span"></div>
+    return <><div className="header">
+        <header className="header__out">
+            <div className="container">
+                <div className="header__hero">
+                    <nav className="header__nav">
+                        <div className="header__toggle">
+                            <div className="header__circle">
+                                <div className="header__circle--span"></div>
+                                <div className="header__circle--span1"></div>
+                                <div className="header__circle--span"></div>
+                            </div>
+                        </div>
+                        <div className="header__logo">
+                            <h2 className="header__logo--logo">LYT</h2>
+                            <p className="header__logo--words">Stay with us!</p>
+                        </div>
+                        <div className="header__buttons">
+                            <button className="header__buttons--register">
+                                Registration
+                            </button>
+                            <Button className="header__buttons--login">Log in</Button>
+                        </div>
+                    </nav>
                 </div>
-              </div>
-              <div className="header__logo">
-                <h2 className="header__logo--logo">LYT</h2>
-                <p className="header__logo--words">Stay with us!</p>
-              </div>
-              <div className="header__buttons">
-                <button className="header__buttons--register">
-                  Registration
-                </button>
-                <Button className="header__buttons--login">Log in</Button>
-              </div>
-            </nav>
-          </div>
-        </div>
-      </header>
-    </>
-  );
+            </div>
+        </header>
+    </div></>;
 };
 
 export default Header;
